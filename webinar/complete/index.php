e9ad14779f3451d1cc98774921e19e254d0caafd
Paul <72017436+paul-baiju@users.noreply.github.com>
2024-03-23 00:16:50 +0530

    paul

---
+++ webinar/complete/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./css/framework.css">
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+    <div class="signin bg-purple">
+            <div class="container">
+                <h1>Welcome to <span>Gyaan Innovation Lab</span></h1>
+                <img src="./images/logo.png"/>
+                <p>Presented by Anuroop K B</p>
+            </div>
+        </div>
+        <script src="" async defer></script>
+    </body>
+</html>
